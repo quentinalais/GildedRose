b819ec4c7a6a57aef749ee417f154282ef76db6f
alaisquentin <quentin.alais@devinci.fr>
2018-11-28 02:04:52 +0100

    running all tests

---
+++ src/Test/java/conjuredCucumberTest/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package conjuredCucumberTest;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict = true)
+public class RunCucumberTest {
+
+}
+
+
